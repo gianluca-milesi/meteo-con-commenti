90022606bf023561b984607f82b6b65e56736a89
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-06-06 13:06:21 +0200

    ADD: Cors and root route

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
-console.log("ciao")
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json("Root");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
