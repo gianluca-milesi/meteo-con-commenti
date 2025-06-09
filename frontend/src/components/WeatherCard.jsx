df8e11ed7c0c23b1271ce8a5d69d268c66f9e087
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-06-09 18:11:52 +0200

    ADD: WeatherCard and fetch weather from OpenWeather API

--- frontend/src/components/WeatherCard.jsx
+++ frontend/src/components/WeatherCard.jsx
@@ -1,8 +1,47 @@
+import { useState, useEffect } from "react"
+const API_KEY = import.meta.env.VITE_API_KEY
+const CITY = "Roma"
+
 function WeatherCard() {
+
+    const [weather, setWeather] = useState(null)
+
+    async function fetchWeather() {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric&lang=it`)
+            if (!response.ok) {
+                throw new Error("Errore nel recupero dei dati meteo")
+            }
+            const data = await response.json()
+            setWeather(data)
+            console.log(data)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [])
+
+    if (!weather) return null
+    const { name, main, weather: weatherDetails } = weather
+    const temperature = Math.round(main.temp)
+    const description = weatherDetails[0].description
+    const icon = weatherDetails[0].icon
+
+
     return (
-        <>
-            <div>Weather Card</div>
-        </>
+        <div className="flex flex-col items-center justify-center text-center space-y-2">
+            <h2 className="text-2xl font-semibold">{name}</h2>
+            <img
+                src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                alt={description}
+                className="w-20 h-20"
+            />
+            <p className="text-xl font-medium">{temperature}°C</p>
+            <p className="text-sm capitalize">{description}</p>
+        </div>
     )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import CommentsSection from "../components/CommentsSection.jsx"
 function HomePage() {
     return (
         <main className="flex justify-center items-center h-screen primary-color">
-            <div className="flex flex-col w-[80vw] h-[80vh] rounded-xl shadow-xl">
+            <div className="flex flex-col max-w-[80vw] h-[80vh] rounded-xl shadow-xl">
                 <div className="flex-1 card-light rounded-tl-xl rounded-tr-xl p-4">
                     <WeatherCard />
                 </div>
